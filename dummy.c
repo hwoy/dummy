2dbd45ae081b2258c9a523f6dba68b13175dfa22
Watthanachai Dueanklang <bosskillerz@gmail.com>
2023-10-26 14:45:30 +0700

    modified:   dummy.c

--- dummy.c
+++ dummy.c
@@ -1,9 +1,10 @@
-#define VERSION 1
+#define MAJOR_VERSION 1
+#define MINOR_VERSION 0
 
 #include <stdio.h>
 
 int main()
 {
-	printf("Dummy version %u\n",VERSION);
+	printf("Dummy version %u.%u\n",MAJOR_VERSION,MINOR_VERSION);
 	return 0;
 }
